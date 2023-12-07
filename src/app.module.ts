d5575edcc95da15c3bced3f091d80c57703e6329
Valentin Sackmann <sava1013@h-ka.de>
2023-12-07 23:14:14 +0100

    set up supertokens

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TherapistModule } from './therapist/therapist.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Therapist } from './therapist/entity/Therapist.entity';
 import { Patient } from './therapist/entity/Patient.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,6 +18,19 @@ import { Patient } from './therapist/entity/Patient.entity';
       entities: [Therapist, Patient],
       //todo set this to fale before production
       synchronize: true,
+    }),
+    AuthModule.forRoot({
+      // https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+      connectionURI: 'https://try.supertokens.com',
+      // apiKey: <API_KEY(if configured)>,
+      appInfo: {
+        // Learn more about this on https://supertokens.com/docs/emailpassword/appinfo
+        appName: 'My queue app',
+        apiDomain: 'http://localhost:3000',
+        websiteDomain: 'http://localhost:3001',
+        apiBasePath: '/auth',
+        websiteBasePath: '/auth',
+      },
     }),
   ],
   controllers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import supertokens from 'supertokens-node';
+import { SupertokensExceptionFilter } from './auth/auth/auth.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //todo implement prefix
   //app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: ['http://localhost:3001'],
+    //methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
     credentials: true,
   });
+
+  app.useGlobalFilters(new SupertokensExceptionFilter());
+
   await app.listen(3001);
 }
 bootstrap();
