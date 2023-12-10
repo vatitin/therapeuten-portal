9eaaedd83a8835585a3696f7c6605850b2e33368
Valentin Sackmann <sava1013@h-ka.de>
2023-12-10 19:11:55 +0100

    configure supertokens dashboard

--- src/auth/supertokens/supertokens.service.ts
+++ src/auth/supertokens/supertokens.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import supertokens from 'supertokens-node';
 import Session from 'supertokens-node/recipe/session';
 import EmailPassword from 'supertokens-node/recipe/emailpassword';
+import Dashboard from 'supertokens-node/recipe/dashboard';
 
 import { ConfigInjectionToken, AuthModuleConfig } from '../config.interface';
 
@@ -11,10 +12,15 @@ export class SupertokensService {
     supertokens.init({
       appInfo: config.appInfo,
       supertokens: {
-        connectionURI: config.connectionURI,
-        apiKey: config.apiKey,
+        connectionURI:
+          'https://st-dev-daa0b600-96b0-11ee-ba63-d36c9a2da200.aws.supertokens.io',
+        apiKey: 'Dkw39OBbn=OhNwg3VCzlC1XuDP',
       },
-      recipeList: [EmailPassword.init(), Session.init()],
+      recipeList: [
+        EmailPassword.init(),
+        Session.init(),
+        Dashboard.init({ admins: ['sackmannva@gmail.com'] }),
+      ],
     });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import supertokens from 'supertokens-node';
 import { SupertokensExceptionFilter } from './auth/auth/auth.filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          'https://cdn.jsdelivr.net/gh/supertokens/',
+        ],
+        imgSrc: ['https://cdn.jsdelivr.net/gh/supertokens/'],
+      },
+    }),
+  );
+
   //todo implement prefix
   //app.setGlobalPrefix('api');
   app.enableCors({
