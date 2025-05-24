5bc46b0e50ec6845e529efd5b7fa9752bb4e92d4
Valentin Sackmann <sackmannva@gmail.com>
2025-05-24 17:06:13 +0200

    add alias for tabler icons to not import the entire library

--- apps/therapist-app/vite.config.ts
+++ apps/therapist-app/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    resolve: {
+    alias: {
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs', // so that not to import the entire library
+    },
+  }
 })
